08c8a42cecb604611f9af5f5a17b6ec27ebd15ca
ammardolovac <ammardolovac@gmail.com>
2023-04-11 16:28:51 +0200

    added new task

---
+++ exercise15/index.js
@@ -0,0 +1,4 @@
+let input = document.getElementById(in1).value;
+let button = document.getElementById(butt).value;
+let display = document.getElementById(dis);
+let div = document.getElementById(mainDiv);
